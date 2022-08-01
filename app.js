e10bfb47f57d44654c98080240dcc4fd694b2bf3
Devin-Jackson314 <dejay314@gmail.com>
2022-08-01 15:24:55 -0400

    adding comments brainstorming

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-//post data
+//get all my buttons by id 
 const $pbutton = $('#pbtn');
 const $deletebtn = $('#deletebtn');
 const $editbtn = $('#editbtn');
+
+//get data 
+
+//post data aka create new post 
+
+//delete data aka delete post 
+
+//patch data edit button 
 
