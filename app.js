dad83fc31836d87dca95cb55a95b3669286ad599
Devin-Jackson314 <dejay314@gmail.com>
2022-08-02 21:52:41 -0400

    is an array returned after first for in loop

--- app.js
+++ app.js
@@ -25,15 +25,17 @@ $gtbtn.click(function () {
                     //" " + userNameKey.lastName + "</p>" + "</div>"
                 //$('#userAppend').append(userGroup)
                 //let userNameKey1 = { ...data }
-                for (const posts in userNameKey) {
-                    if (Object.hasOwnProperty.call(userNameKey, posts)) {
-                        const feed = userNameKey[posts];
-                        //console.log(feed)
+
+                //is feed an array now because of previous loop 
+                for (const post in userNameKey) {
+                    if (Object.hasOwnProperty.call(userNameKey, post)) {
+                        const feed = userNameKey[post];
+                        console.log(feed)
                         let userGroup2 = "div class = 'text-box' style= 'border: 1px solid black; margin-top: 5 px'>" +
                             "<p>" + userNameKey.firstName + " " + userNameKey.lastName +
                             "</p>" + "<p>" + feed.post + "</p>" +
                             "<p>" + feed.time + "</p>" + "</div>"
-                        $('#userList').append(userBox2)
+                        $('#userList').append(userGroup2)
                   
                     }
                 }
