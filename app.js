c8514538fe1f01a71913f414613ad837b07d1c37
Devin-Jackson314 <dejay314@gmail.com>
2022-08-03 21:16:22 -0400

    iffy now working

--- app.js
+++ app.js
@@ -9,54 +9,49 @@ let jsonEXT = ".json";
 
 
 //get method
-$gtbtn.click(function () {
+//$gtbtn.click
+(function () {
     $.ajax({
         type: "GET",
         url: `${fireBaseUrl}${jsonEXT}`,
         success: function (data) {
-            //console.log(data, "you have logged your data");
+            console.log(data, "you have logged your data");
             let userInfo = { ...data }
-            for (const userName in userInfo) { 
+            for (const userName in userInfo) {
                 if (Object.hasOwnProperty.call(userInfo, userName)) {
                     const userNameKey = userInfo[userName];
-                    //console.log(userNameKey, "This is the user name key")
-                    //console.log(userNameKey.firstName)
-                    //let userGroup = "<div>" + "<p>" + userNameKey.firstName +
-                    //" " + userNameKey.lastName + "</p>" + "</div>"
-                    //$('#userAppend').append(userGroup)
-                    //let userNameKey1 = { ...data }
-
+                    console.log(userNameKey, "This is the user name key")
+                    console.log(userNameKey.firstName)
+                    let userGroup = "<div>" + "<p>" + userNameKey.firstName +
+                        " " + userNameKey.lastName + "</p>" + "</div>"
+                    $('#userAppend').append(userGroup)
+                    let userNameKey1 = { ...data }
                     let userInfo2 = { ...data }
                     for (const id in userInfo2) {
                         if (Object.hasOwnProperty.call(userInfo2, id)) {
                             const feed = userInfo2[id].posts;
                             console.log(feed, "this is the feed")
-
-                           
-
-                         for (const post in feed) {
-                            if (Object.hasOwnProperty.call(feed,post)) {
-                                const postinfo = feed[post];
-                                console.log(postinfo, "This is your post info");
-                                console.log(postinfo.id, "This is your post id");
-                                console.log(postinfo.time, "This is the time");
-                                console.log(postinfo.post, "This is your post title")
-                                
-                            let userGroup2 = "<div class = 'text-box' style= 'border: 1px solid black; margin-top: 5 px'>" +
-                             "<p>" + userNameKey.firstName + " " + userNameKey.lastName +
-                             "</p>" + "<p>" + postinfo.post + "</p>" +
-                            "<p>" + postinfo.time + "</p>" + "</div>"
-                            $('#userAppend').append(userGroup2)
+                        
+                            for (const post in feed) {
+                                if (Object.hasOwnProperty.call(feed, post)) {
+                                    const postinfo = feed[post];
+                                    console.log(postinfo, "This is your post info");
+                                    console.log(postinfo.id, "This is your post id");
+                                    console.log(postinfo.time, "This is the time");
+                                    console.log(postinfo.post, "This is your post title")
+                            
+                                    let userGroup2 = "<div class = 'text-box' style= 'border: 1px solid black; margin-top: 5 px'>" +
+                                        "<p>" + userNameKey.firstName + " " + userNameKey.lastName +
+                                        "</p>" + "<p>" + postinfo.post + "</p>" +
+                                        "<p>" + postinfo.time + "</p>" + "</div>"
+                                    $('#userAppend').append(userGroup2)
                                
-                                
+                                }
                             }
-                         }
                   
                         }
                     }
-
-                    
-            }
+                }
             }
            
         },
@@ -64,8 +59,50 @@ $gtbtn.click(function () {
             console.log(error);
         }
     })
-})
-//get data func
+}());
+
+
+//get data IFFY
+// (function(){
+//     $.ajax({
+//         type: "GET",
+//         url: `${fireBaseUrl}${jsonEXT}`,
+//         success: function (data) {
+//             let userInfo = { ...data }
+//             for (const userName in userInfo) {
+//                 if (Object.hasOwnProperty.call(userInfo, userName)) {
+//                     const userNameKey = userInfo[userName];
+
+//                     let userInfo2 = { ...data }
+//                     for (const id in userInfo2) {
+//                         if (Object.hasOwnPropery.call(userInfo2, id)) {
+//                             const feed = userInfo2[id].posts;
+
+//                             for (const post in feed) {
+//                                 if (Object.hasOwnProperty.call(feed, post)) {
+//                                     const postinfo = feed[post];
+
+//                                     let userGroup2 = "<div class = 'text-box' style= 'border: 1px solid black; margin-top: 5 px'>" +
+//                                         "<p>" + userNameKey.firstName + " " + userNameKey.lastName +
+//                                         "</p>" + "<p>" + postinfo.post + "</p>" +
+//                                         "<p>" + postinfo.time + "</p>" + "</div>";
+//                                     $('#userAppend').append(userGroup2);
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+            
+
+            
+//         },
+//         error: function(error){
+//             console.log(error)
+//         }
+//     })
+// })();
+
 
      
 
